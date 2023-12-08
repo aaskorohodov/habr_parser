9de5a81b3edcde5be0c0035eb790e085665477af
DY <aaskorohodov@gmail.com>
2023-12-08 18:52:21 +0500

    0.3 Alpha bugfix

--- parser_app/main.py
+++ parser_app/main.py
@@ -10,20 +10,16 @@ from parser_app.utils.env_setter import EnvSetter
 
 
 if __name__ == '__main__':
-    EnvSetter.set_envs(ENV_FILE_PATH)
-    logger = STDLogger
-    main_controller = MainController(PARSER_DB_CHECK_INTERVAL, logger)
-    main_controller.start()
-    # while True:
-    #     try:
-    #         EnvSetter.set_envs(ENV_FILE_PATH)
-    #         logger = STDLogger
-    #         main_controller = MainController(PARSER_DB_CHECK_INTERVAL, logger)
-    #         main_controller.start()
-    #     except Exception as e:
-    #         print(f'An error occurred, during running parser-application:'
-    #               f'\n\n'
-    #               f'{e.__str__()}'
-    #               f'\n\n'
-    #               f'Restarting in 10 seconds...')
-    #         time.sleep(10)
+    while True:
+        try:
+            EnvSetter.set_envs(ENV_FILE_PATH)
+            logger = STDLogger
+            main_controller = MainController(PARSER_DB_CHECK_INTERVAL, logger)
+            main_controller.start()
+        except Exception as e:
+            print(f'An error occurred, during running parser-application:'
+                  f'\n\n'
+                  f'{e.__str__()}'
+                  f'\n\n'
+                  f'Restarting in 10 seconds...')
+            time.sleep(10)
